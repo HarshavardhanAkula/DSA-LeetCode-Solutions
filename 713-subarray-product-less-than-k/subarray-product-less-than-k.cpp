b17fa9076047baa77afd22bcc521fb370688c1fe
Harshavardhan Akula <harsha665695@gmail.com>
2026-09-07 22:13:30 +0530

    Time: 4 ms (22.36%) | Memory: 44.9 MB (5.79%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -14,7 +14,7 @@ public:
                 prod/=nums[left];
                 left++;
             }
-            count+=right-left+1;
+            count+=(right-left+1);
         }
         return count;
   
